5f5fe40dac2adf10fd11e0b7c70f71f3898bdd68
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-09-15 20:09:36 +0300

    Counter

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.scss';
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  const onClickPlus = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  }
+
+  const onClickMinus = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
